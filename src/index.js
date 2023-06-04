bb5bd6252bb55252ff5ad7cbf626099e25b5a23e
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-06-04 15:00:28 +0300

    fixes routes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import { store } from './store'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
