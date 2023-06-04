a8748f8859d2cafc9d01cd9a767de36cce7380bf
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-06-04 14:16:09 +0300

    fixes

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const path = 'http://localhost:8080';
+// const path = 'http://localhost:8080';
+const path = 'https://delivery-node-js.herokuapp.com';
 const sessionId = window.localStorage.getItem('sessionID');
 
 export const getShopsItems = () => {
